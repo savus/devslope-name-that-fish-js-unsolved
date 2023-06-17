11526ac78ebab47a60e5e7603bc8727f04eeaddc
Savus <justin.fuzzygreenstuff@gmail.com>
2023-06-17 03:19:06 -0600

    Clone repo and establish state variables and setters to pass through FunctionalGameBoard component.

--- src/Components/Functional/FunctionalApp.jsx
+++ src/Components/Functional/FunctionalApp.jsx
@@ -1,12 +1,44 @@
 import { FunctionalGameBoard } from "./FunctionalGameBoard";
 import { FunctionalScoreBoard } from "./FunctionalScoreBoard";
 import { FunctionalFinalScore } from "./FunctionalFinalScore";
+import { useState } from "react";
+import { Images } from "../../assets/Images";
+
+const initialFishes = [
+  {
+    name: "trout",
+    url: Images.trout,
+  },
+  {
+    name: "salmon",
+    url: Images.salmon,
+  },
+  {
+    name: "tuna",
+    url: Images.tuna,
+  },
+  {
+    name: "shark",
+    url: Images.shark,
+  },
+];
 
 export function FunctionalApp() {
+  const [userInformation, setUserInformation] = useState({
+    incorrect:0,
+    correct:0,
+    index:0
+  })
   return (
     <>
       <FunctionalScoreBoard />
-      <FunctionalGameBoard />
+      <FunctionalGameBoard 
+        initialFishes={initialFishes}
+        userInformation={userInformation}
+        handleUserInformation={(userInformation) => {
+          setUserInformation(userInformation)
+        }}
+      />
       {false && <FunctionalFinalScore />}
     </>
   );
